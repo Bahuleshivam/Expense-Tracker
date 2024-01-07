f146ff74eb13cf5c1e60fbc35dde37502bf6040f
Shivam Bahule <bahuleshivam20@gmail.com>
2024-01-07 20:42:52 +0530

    first commit

---
+++ src/components/notes/Notes.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import '../notes/notes.css'
+import { MyContext } from '../../context/MyContext';
+import { useContext } from 'react';
+import { DeleteForever } from '@mui/icons-material';
+
+
+const Notes = () => {
+
+  const { income, expense } = useContext(MyContext);
+
+  return (
+    <div className='notes-container'>
+      <div className="notes-box">
+
+        <div className="notes-box-income">
+          <h1 className='notes-heading income-bg-color'>Income</h1>
+
+          {income.length === 0 ? (<p> No Data To Show</p>) : (
+
+            income.map((item, index) => {
+
+              return <div className='todo' key={index}>
+                <div className="subDiv">
+                  <p>{item.description}</p>
+                  <h3> ₹ {item.amount}</h3>
+                </div>
+                <DeleteForever className='note-delete-btn' />
+              </div>
+            })
+          )}
+
+        </div>
+
+        <div className="notes-box-expense">
+          <h1 className='notes-heading expense-bg-color'>Expense</h1>
+
+          {expense.length === 0 ? (<p>No Data To Show</p>) : (
+
+            expense.map((item, index) => {
+
+              return <div className='todo' key={index}>
+                <div className="subDiv">
+                  <p style={{ width: '60%' }} >{item.description}</p>
+                  <h3> ₹ {item.amount}</h3>
+                </div>
+                <DeleteForever className='note-delete-btn' />
+              </div>
+            })
+
+          )}
+
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Notes;
